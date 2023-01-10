1567ca242c2a5820e8b8ceffe7329c3a061b615f
Carlos Fernandez <carlosfernandez11798@gmail.com>
2023-01-09 19:36:28 -0500

    Pagina Ucompensar

---
+++ VideotecaCrai/app.js
@@ -0,0 +1,23 @@
+document.querySelector('.menu-btn').addEventListener('click', ()=> {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+(function(){
+	$(document).ready(function(){
+		$(".btn-menu").click(function(e){
+			e.preventDefault();
+			var filtro = $(this).attr("data-filter");
+
+			if (filtro == "todos") {
+				$(".box-img").show(500);
+			} else {
+				$(".box-img").not("."+filtro).hide(500);
+				$(".box-img").filter("."+filtro).show(500);
+			}
+		});
+
+		$("ul li").click(function(){
+			$(this).addClass("active").siblings().removeClass("active");
+		});
+	});
+}())
